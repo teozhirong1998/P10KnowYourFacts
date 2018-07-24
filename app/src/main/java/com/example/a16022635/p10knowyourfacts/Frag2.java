7eb8de19e9acc14b5e81080762790b80eaec4324
teozhirong1998 <16022635@myrp.edu.sg>
2018-07-24 15:34:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16022635/p10knowyourfacts/Frag2.java
@@ -0,0 +1,48 @@
+package com.example.a16022635.p10knowyourfacts;
+
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Frag2 extends Fragment {
+
+    Button btnChgColor;
+    TextView tv;
+    public Frag2() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_2, container, false);
+
+        btnChgColor = (Button) view.findViewById(R.id.btnChgColor2);
+        tv = (TextView) view.findViewById(R.id.tv2);
+        btnChgColor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rnd = new Random();
+                int color = Color.argb(255, rnd.nextInt(256), rnd.nextInt(256), rnd.nextInt(256));
+                tv.setBackgroundColor(color);
+            }
+        });
+
+        return view;
+    }
+
+}
---
+++ app/src/main/java/com/example/a16022635/p10knowyourfacts/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.a16022635.p10knowyourfacts;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Fragment> al;
+    MyFragmentPagerAdapter adapter;
+    ViewPager vPager;
+    Button btnReadLater;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        vPager = (ViewPager) findViewById(R.id.viewpager1);
+        btnReadLater = (Button) findViewById(R.id.btnReadLater);
+        FragmentManager fm = getSupportFragmentManager();
+
+        al = new ArrayList<Fragment>();
+        al.add(new Frag1());
+        al.add(new Frag2());
+        al.add(new Frag3());
+
+        adapter = new MyFragmentPagerAdapter(fm, al);
+
+        vPager.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_previous:
+                if(vPager.getCurrentItem() > 0) {
+                    int previousPage = vPager.getCurrentItem() - 1;
+                    vPager.setCurrentItem(previousPage, true);
+                }
+                return true;
+
+            case R.id.action_random:
+
+                Random random = new Random();
+                vPager.setCurrentItem(random.nextInt(3), true);
+
+                return true;
+
+            case R.id.action_next:
+                int max = vPager.getChildCount();
+                if(vPager.getCurrentItem() < max-1) {
+                    int nextPage = vPager.getCurrentItem() + 1;
+                    vPager.setCurrentItem(nextPage, true);
+                }
+            default:
+                return super.onOptionsItemSelected(item);
+        }    }
+}
